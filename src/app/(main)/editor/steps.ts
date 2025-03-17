e005fc9d2242063d8d3c56c0b1c07f94fadc33b4
AlexKasema <lexxmulwa@gmail.com>
2025-03-17 14:50:40 +0300

    Created the work experience form component. Used useFieldArray from react-hook-form to enable a user add or remove work experiences dynamically.

--- src/app/(main)/editor/steps.ts
+++ src/app/(main)/editor/steps.ts
@@ -1,6 +1,7 @@
 import { EditorFormProps } from "@/lib/types";
 import GeneralInfoForm from "./forms/GeneralInfoForm";
 import PersonalInfoForm from "./forms/PersonalInfoForm";
+import WorkExperienceForm from "./forms/WorkExperienceForm";
 
 //! an array of objects
 //! each object has a title, component(The form we want to render), and key(what we put to the url to open the form when we refresh a page)
@@ -11,4 +12,9 @@ export const steps: {
 }[] = [
   { title: "General info", component: GeneralInfoForm, key: "general-info" },
   { title: "Personal info", component: PersonalInfoForm, key: "personal-info" },
+  {
+    title: "Work experience",
+    component: WorkExperienceForm,
+    key: "work-experience",
+  },
 ];
--- src/lib/validation.ts
+++ src/lib/validation.ts
@@ -37,11 +37,28 @@ export const personalInfoSchema = z.object({
 
 export type PersonalInfoValues = z.infer<typeof personalInfoSchema>;
 
+export const workExperienceSchema = z.object({
+  workExperiences: z
+    .array(
+      z.object({
+        position: optionalString,
+        company: optionalString,
+        startDate: optionalString,
+        endDate: optionalString,
+        description: optionalString,
+      }),
+    )
+    .optional(),
+});
+
+export type WorkExperienceValues = z.infer<typeof workExperienceSchema>;
+
 //! We can merge two schemas together using the shape property
 //! This way we can create a schema that includes all the fields from all schemas
 export const resumeSchema = z.object({
   ...generalInfoSchema.shape,
   ...personalInfoSchema.shape,
+  ...workExperienceSchema.shape,
 });
 
 //! Extend the type because when we update an already existing resume we have an id field
