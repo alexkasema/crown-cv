2ca3ea08efff93222d0d10ca4f92698907fd16c3
AlexKasema <lexxmulwa@gmail.com>
2025-03-18 23:57:37 +0300

    Implemented fetching of the resumes from the DB. A user is able to view their resumes and click on then to go edit.

--- src/app/(main)/resumes/page.tsx
+++ src/app/(main)/resumes/page.tsx
@@ -1,14 +1,51 @@
 import { Metadata } from "next";
 import CreateResumeButton from "./CreateResumeButton";
+import { auth } from "@clerk/nextjs/server";
+import prisma from "@/lib/prisma";
+import { resumeDataInclude } from "@/lib/types";
+import ResumeItem from "./ResumeItem";
 
 export const metadata: Metadata = {
   title: "Your resumes",
 };
 
-const Resumes = () => {
+const Resumes = async () => {
+  const { userId } = await auth();
+
+  if (!userId) {
+    return null;
+  }
+
+  const [resumes, totalCount] = await Promise.all([
+    prisma.resume.findMany({
+      where: {
+        userId,
+      },
+      orderBy: {
+        updatedAt: "desc",
+      },
+      include: resumeDataInclude,
+    }),
+    prisma.resume.count({
+      where: {
+        userId,
+      },
+    }),
+    //TODO check quota for non premium users
+  ]);
+
   return (
     <main className="mx-auto w-full max-w-7xl space-y-6 px-3 py-6">
       <CreateResumeButton canCreate={true} />
+      <div className="space-y-1">
+        <h1 className="text-3xl font-bold">Your resumes</h1>
+        <p>Total: {totalCount}</p>
+      </div>
+      <div className="flex w-full grid-cols-2 flex-col gap-3 sm:grid md:grid-cols-3 lg:grid-cols-4">
+        {resumes.map((resume) => (
+          <ResumeItem key={resume.id} resume={resume} />
+        ))}
+      </div>
     </main>
   );
 };
